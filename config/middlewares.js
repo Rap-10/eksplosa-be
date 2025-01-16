066eb7623742c0b67a262afac708c0cda483f5a4
Rap-10 <rapwork10@gmail.com>
2025-01-16 19:39:23 +0700

    fix: cors

--- config/middlewares.js
+++ config/middlewares.js
@@ -2,15 +2,7 @@ module.exports = [
   'strapi::logger',
   'strapi::errors',
   'strapi::security',
-  // 'strapi::cors',
-  {
-    name: 'strapi::cors',
-    config: {
-      origin: ['http://127.0.0.1:5500', 'https://1772-202-57-27-81.ngrok-free.app/'],
-      method: ['GET', 'POST', 'PUT', 'DELETE'],
-      headers: ['Content-Type']
-    }
-  },
+  'strapi::cors',
   'strapi::poweredBy',
   'strapi::query',
   'strapi::body',
